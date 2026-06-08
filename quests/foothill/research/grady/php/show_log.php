49ddbb993af658c93b66ab487a37ce3f8bdfd7dd
Anand <venkatraraman.anand@gmail.com>
2026-06-08 15:47:15 +0530

    Removed the quests subdir now that's in the Release distro

--- quests/foothill/research/grady/php/show_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// This dir is on EFS and will survive crashes
-$logfile = "/var/www/quests.nonlinearmedia.org/logs/grady-log.csv";
-
-$lines = file($logfile);
-echo "<PRE>", implode($lines), "</PRE>";
-
-?>
--- quests/genius/unlisted/b102c3297c5fe1cf578cd34d09254d92/get-genius-wl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$datafile = "/var/www/quests.nonlinearmedia.org/subs/scores/genius/waitlist.tsv";
-
-$lines = file($datafile);
-echo "<PRE>", implode($lines), "</PRE>";
-
-?>
